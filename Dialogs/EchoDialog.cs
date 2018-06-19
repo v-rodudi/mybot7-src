2ef30068648e7c6aae158a8771668b407209938d
v-rodudi <v-rodudi@microsoft.com>
2018-06-19 15:43:26 +0300

    Added bot example.

---
+++ Dialogs/EchoDialog.cs
@@ -0,0 +1,58 @@
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace Microsoft.Bot.Sample.SimpleEchoBot
+{
+    [Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        protected int count = 1;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+
+            switch (message.Text)
+            {
+                case "reset":
+                    PromptDialog.Confirm(
+                                        context,
+                                        AfterResetAsync,
+                                        "Are you sure you want to reset the count?",
+                                        "Didn't get that!",
+                                        promptStyle: PromptStyle.Auto);
+                    break;
+                default:
+
+                    await context.PostAsync($"{this.count++}: You said {message.Text}");
+                    context.Wait(MessageReceivedAsync);
+                    break;
+            }
+        }
+
+        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                this.count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+
+    }
+}
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=mybot7-946c-Web-Deploy.pubxml -p:Password=cP7HtYcdJ80liX6vYcZalTnyaraxHebAFsbz2kcbv8nRNo6bL96HitCv9Q90
+
